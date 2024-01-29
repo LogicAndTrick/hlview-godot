de5b324fb5e08002c7ddc5e229561c8f53fdf76f
Daniel Walder <daniel.walder@gmail.com>
2024-01-29 17:11:35 +1000

    Commit before trying lightmap stuff

--- Net/BspNode.cs
+++ Net/BspNode.cs
@@ -1,7 +1,14 @@
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.IO;
+using System.Reflection;
+using Godot;
 using Sledge.Formats.Bsp;
+using Sledge.Formats.Bsp.Lumps;
 using Sledge.Formats.Bsp.Objects;
+using Sledge.Formats.Id;
+using Color = Godot.Color;
 using FileAccess = System.IO.FileAccess;
 
 namespace HLView.Net;
@@ -97,50 +104,79 @@ public partial class BspNode : Node3D
             return ta.MipTexture.CompareTo(tb.MipTexture);
         });
 
-        foreach (var face in faces.Take(200))
+        foreach (var faceGroup in faces.GroupBy(x => bsp.Texinfo[x.TextureInfo].MipTexture))
         {
-            var plane = bsp.Planes[face.Plane];
-            var texInfo = bsp.Texinfo[face.TextureInfo];
-            var mipTexture = bsp.Textures[texInfo.MipTexture];
-
-            var vertices = new List<LightmappedVertex>();
-
-            // map the vertices
-            for (var j = face.FirstEdge; j < face.FirstEdge + face.NumEdges; j++)
-            {
-                var edgeIndex = bsp.Surfedges[j];
-                var edge = bsp.Edges[Math.Abs(edgeIndex)];
-
-                var vertexIndex = edgeIndex > 0 ? edge.Start : edge.End;
-                var vertex = bsp.Vertices[vertexIndex].ToGodotVector3();
-
-                var normal = plane.Normal.ToGodotVector3();
-
-                var sn = texInfo.S.ToGodotVector3();
-                var u = vertex.Dot(sn) + texInfo.S.W;
-
-                var tn = texInfo.T.ToGodotVector3();
-                var v = vertex.Dot(tn) + texInfo.T.W;
-
-                // todo lightmaps
-                vertices.Add(new LightmappedVertex
-                {
-                    Position = vertex,
-                    Normal = normal,
-                    Texture = new Vector2(u, v),
-                    Color = Colors.White,
-                    Lightmap = default
-                });
-            }
-
+            var mipTexture = bsp.Textures[faceGroup.Key];
             var mipTextureSize = new Vector2(mipTexture.Width, mipTexture.Height);
+            var builder = new LightmapBuilder();
+
+            var mappedFaces = new List<LightmappedFace>();
+
+            foreach (var face in faceGroup)
+            {
+                var mappedFace = new LightmappedFace();
+                mappedFaces.Add(mappedFace);
+
+                var plane = bsp.Planes[face.Plane];
+                var texInfo = bsp.Texinfo[face.TextureInfo];
+
+                // map the vertices
+                for (var j = face.FirstEdge; j < face.FirstEdge + face.NumEdges; j++)
+                {
+                    var edgeIndex = bsp.Surfedges[j];
+                    var edge = bsp.Edges[Math.Abs(edgeIndex)];
+
+                    var vertexIndex = edgeIndex > 0 ? edge.Start : edge.End;
+                    var vertex = bsp.Vertices[vertexIndex].ToGodotVector3();
+
+                    var normal = plane.Normal.ToGodotVector3();
+
+                    var sn = texInfo.S.ToGodotVector3();
+                    var u = vertex.Dot(sn) + texInfo.S.W;
+
+                    var tn = texInfo.T.ToGodotVector3();
+                    var v = vertex.Dot(tn) + texInfo.T.W;
+
+                    // todo lightmaps
+                    mappedFace.Vertices.Add(new LightmappedVertex
+                    {
+                        Position = vertex,
+                        Normal = normal,
+                        Texture = new Vector2(u, v), // needs to be divided by the texture dimensions
+                        Color = Colors.White,
+                        Lightmap = default
+                    });
+                }
+
+                var minu = mappedFace.Vertices.Min(x => x.Texture.X);
+                var maxu = mappedFace.Vertices.Max(x => x.Texture.X);
+                var minv = mappedFace.Vertices.Min(x => x.Texture.Y);
+                var maxv = mappedFace.Vertices.Max(x => x.Texture.Y);
+                var extentH = maxu - minu;
+                var extentV = maxv - minv;
+
+                var mapWidth = (int)Math.Ceiling(maxu / 16) - (int)Math.Floor(minu / 16) + 1;
+                var mapHeight = (int)Math.Ceiling(maxv / 16) - (int)Math.Floor(minv / 16) + 1;
+                /*
+                var lightmap = bsp.Lightmaps[face.]
+                if (face.LightmapOffset < 0 || face.LightmapOffset >= Bsp.Lightmap.Length || face.Styles[0] == byte.MaxValue)
+                {
+                    // fullbright
+                    mappedFace.LightmapAllocation = builder.FullbrightRectangle;
+                }
+                else
+                {
+                    mappedFace.LightmapAllocation = builder.Allocate(mapWidth, mapHeight, bsp.Lightmaps, face.LightmapOffset);
+                }
+                */
+            }
 
             var surfaceTool = new SurfaceTool();
             surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
 
             // create the material
             var material = new StandardMaterial3D();
-            material.AlbedoTexture = _textures[texInfo.MipTexture];
+            material.AlbedoTexture = _textures[faceGroup.Key];
             material.AOOnUV2 = true;
             material.AOEnabled = true;
             // material.AOTexture
@@ -148,29 +184,39 @@ public partial class BspNode : Node3D
             _materials.Add(material);
             surfaceTool.SetMaterial(material);
 
-            // triangulate the face
-            for (var j = 1; j < face.NumEdges - 1; j++)
+            // triangulate the faces
+            foreach (var face in mappedFaces)
             {
-                var a = vertices[0];
-                var c = vertices[j + 1];
-                var b = vertices[j];
+                for (var j = 1; j < face.Vertices.Count - 1; j++)
+                {
+                    var a = face.Vertices[0];
+                    var c = face.Vertices[j + 1];
+                    var b = face.Vertices[j];
 
-                surfaceTool.SetUV(a.Texture / mipTextureSize);
-                surfaceTool.SetNormal(a.Normal);
-                surfaceTool.AddVertex(a.Position);
+                    surfaceTool.SetUV(a.Texture / mipTextureSize);
+                    surfaceTool.SetNormal(a.Normal);
+                    surfaceTool.AddVertex(a.Position);
 
-                surfaceTool.SetUV(b.Texture / mipTextureSize);
-                surfaceTool.SetNormal(b.Normal);
-                surfaceTool.AddVertex(b.Position);
+                    surfaceTool.SetUV(b.Texture / mipTextureSize);
+                    surfaceTool.SetNormal(b.Normal);
+                    surfaceTool.AddVertex(b.Position);
 
-                surfaceTool.SetUV(c.Texture / mipTextureSize);
-                surfaceTool.SetNormal(c.Normal);
-                surfaceTool.AddVertex(c.Position);
+                    surfaceTool.SetUV(c.Texture / mipTextureSize);
+                    surfaceTool.SetNormal(c.Normal);
+                    surfaceTool.AddVertex(c.Position);
+                }
             }
 
             surfaceTool.Commit(mesh);
         }
     }
+
+    private class LightmappedFace
+    {
+        public List<LightmappedVertex> Vertices { get; } = new();
+        public Rectangle LightmapAllocation { get; set; }
+    }
+
     private class LightmappedVertex
     {
         public Vector3 Position;
